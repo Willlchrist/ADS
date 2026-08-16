5537a31d678c860a96fe5b1b2d5072afd422a4f9
Willlchrist <60238450+Willlchrist@users.noreply.github.com>
2026-08-15 22:03:28 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/ADS/',
   plugins: [react()],
@@ -13,5 +12,9 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 });
